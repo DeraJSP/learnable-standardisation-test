5e897d6aa0582a502517c978832edd4f117cf493
DeraJSP <derajsp@gmail.com>
2024-06-03 21:56:51 +0100

    product page

--- src/pages/landing_page/ProductDetails.jsx
+++ src/pages/landing_page/ProductDetails.jsx
@@ -1,5 +1,6 @@
 import { useGetProductDetailsQuery } from "./productsApi";
 import { useParams } from "react-router-dom";
+import "./ProductDetails.css";
 
 const ProductDetails = () => {
   const { id } = useParams();
@@ -14,9 +15,33 @@ const ProductDetails = () => {
   }
 
   return (
-    <div>
-      <h1>{data.title}</h1>
-      <p>{data.description}</p>
+    <div className="product-view">
+      <img className="product-img" src={data.images} alt={data.title} />
+      <div className="product-details">
+        <h4>{data.title}</h4>
+        <div className="rating">
+          <img src={require("../../assets/landing_page/stars.svg")} alt="" />
+          <h6>10 Reviews</h6>
+        </div>
+        <h3>${data.price}</h3>
+
+        <h6>
+          Availability:&nbsp;
+          <span>{data.availabilityStatus}</span>
+        </h6>
+        <div className="colors">
+          <div className="blue"></div>
+          <div className="green"></div>
+          <div className="orange"></div>
+          <div className="black"></div>
+        </div>
+        <button>
+          <span>Select Options</span>
+        </button>
+        <img src={require("../../assets/landing_page/phone.svg")} alt="" />
+        <img src={require("../../assets/landing_page/phone.svg")} alt="" />
+        <img src={require("../../assets/landing_page/phone.svg")} alt="" />
+      </div>
     </div>
   );
 };
