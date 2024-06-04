2fe2b0a1cd684b055e3be355457d546f8453732e
DeraJSP <derajsp@gmail.com>
2024-06-04 11:53:38 +0100

    fixed checkout page

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -2,6 +2,8 @@
 
 import { useSelector, useDispatch } from "react-redux";
 import { increment, decrement, removeFromCart } from "./CartSlice";
+import NavBar from "../landing_page/NavBar";
+import Footer from "../landing_page/Footer";
 
 function Cart() {
   const cartItems = useSelector((state) => state.cart.items);
@@ -17,99 +19,105 @@ function Cart() {
   console.log(cartItems);
 
   return (
-    <main>
-      <section>
-        <h1>Shopping Cart</h1>
-        <table>
-          <thead>
-            <tr>
-              <th>Item Detail</th>
-              <th>Quantity</th>
-              <th>Price</th>
-            </tr>
-          </thead>
-          <tbody>
-            {cartItems.map((item) => (
-              <tr key={item.id}>
-                <td>
-                  <div>
-                    <img src={item.thumbnail} alt={item.title} />
+    <>
+      <NavBar />
+      <main>
+        <section>
+          <h1>Shopping Cart</h1>
+          <table>
+            <thead>
+              <tr>
+                <th>Item Detail</th>
+                <th>Quantity</th>
+                <th>Price</th>
+              </tr>
+            </thead>
+            <tbody>
+              {cartItems.map((item) => (
+                <tr key={item.id}>
+                  <td>
                     <div>
-                      <h3>{item?.text}</h3>
-                      <p>{item.availabilityStatus}</p>
-                      <p>
-                        Rating: {item.rating} ({item.numReviews} reviews)
-                      </p>
+                      <img src={item.thumbnail} alt={item.title} />
+                      <div>
+                        <h3>{item.title}</h3>
+                        <p>{item.availabilityStatus}</p>
+                        <p>
+                          Rating: {item.rating} ({item.numReviews} reviews)
+                        </p>
+                        <button
+                          onClick={() =>
+                            dispatch(removeFromCart({ id: item.id }))
+                          }
+                        >
+                          <i className="fas fa-trash"></i> Remove
+                        </button>
+                      </div>
+                    </div>
+                  </td>
+                  <td>
+                    <div>
                       <button
-                        onClick={() =>
-                          dispatch(removeFromCart({ id: item.id }))
-                        }
+                        onClick={() => dispatch(decrement({ id: item.id }))}
                       >
-                        <i className="fas fa-trash"></i> Remove
+                        -
+                      </button>
+                      <span>{item.quantity}</span>
+                      <button
+                        onClick={() => dispatch(increment({ id: item.id }))}
+                      >
+                        +
                       </button>
                     </div>
-                  </div>
-                </td>
-                <td>
-                  <div>
-                    <button
-                      onClick={() => dispatch(decrement({ id: item.id }))}
-                    >
-                      -
-                    </button>
-                    <span>{item.quantity}</span>
-                    <button
-                      onClick={() => dispatch(increment({ id: item.id }))}
-                    >
-                      +
-                    </button>
-                  </div>
-                </td>
-                <td>
-                  <p>${(item.price * item.quantity).toFixed(2)}</p>
-                  <p>({item.price?.toFixed(2)} x 1 item)</p>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </section>
+                  </td>
+                  <td>
+                    <p>${(item.price * item.quantity).toFixed(2)}</p>
+                    <p>({item.price?.toFixed(2)} x 1 item)</p>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </section>
 
-      <section className="cartSummary">
-        <div className="orderSummaryDiv">
-          <div className="orderSummary1">
-            <h3>Order Summary</h3>
-            <p>{totalQuantity} items</p>
+        <section className="cartSummary">
+          <div className="orderSummaryDiv">
+            <div className="orderSummary1">
+              <h3>Order Summary</h3>
+              <p>{totalQuantity} items</p>
+            </div>
+            <div className="deliveryCharges">
+              <h5>Delivery Charges</h5>
+              <p>
+                Add your delivery address to checkout to see delivery charges
+              </p>
+            </div>
           </div>
-          <div className="deliveryCharges">
-            <h5>Delivery Charges</h5>
-            <p>Add your delivery address to checkout to see delivery charges</p>
+          <div className="subTotalDiv">
+            <div className="subTotal">
+              <h5>SubTotal</h5>
+              <p>${calculateTotalPrice()}</p>
+            </div>
+            <div className="subTotal">
+              <h4>Total</h4>
+              <p className="totalPrice">${calculateTotalPrice()}</p>
+            </div>
+            <div className="subTotalLastPDiv">
+              <p>Excluding Delivery Charges</p>
+            </div>
           </div>
-        </div>
-        <div className="subTotalDiv">
-          <div className="subTotal">
-            <h5>SubTotal</h5>
-            <p>${calculateTotalPrice()}</p>
-          </div>
-          <div className="subTotal">
-            <h4>Total</h4>
-            <p className="totalPrice">${calculateTotalPrice()}</p>
-          </div>
-          <div className="subTotalLastPDiv">
-            <p>Excluding Delivery Charges</p>
-          </div>
-        </div>
 
-        <div className="checkoutButton">
-          <button>Proceed to Checkout</button>
-          <div>
-            <img src="" alt="" />
-            <img src="" alt="" />
-            <img src="" alt="" />
+          <div className="checkoutButton">
+            <button>Proceed to Checkout</button>
+            <div>
+              <img src="" alt="" />
+              <img src="" alt="" />
+              <img src="" alt="" />
+            </div>
           </div>
-        </div>
-      </section>
-    </main>
+        </section>
+      </main>
+      <Footer />
+    </>
   );
 }
 
